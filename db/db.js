2d36593d1a02d73df55c7b7ebe5266c3fa3cde3f
mariyabaig <mariyabaig0106@gmail.com>
2023-06-09 11:40:47 +0530

    fix : mongodb connection fixed

--- db/db.js
+++ db/db.js
@@ -1,14 +1,16 @@
+
 const mongoose = require("mongoose");
-const DB = process.env.DB
+require("dotenv").config();
 
-mongoose.connect(DB,
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  }
-);
+const DB = process.env.DB;
 
-const db = mongoose.connection
-db.on('error',(error)=> console.log(error))
-db.once("open",()=>console.log("Connected to the DB!"))
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to the DB!"));
+
+module.exports = db;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = process.env.PORT || 4000
 const mongoose= require("mongoose")
 const session = require("express-session")
+const db = require("./db/db");
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,16 +13,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-const DB = process.env.DB
-
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  
-});
-;
-
-const db = mongoose.connection
-db.on('error',(error)=> console.log(error))
-db.once("open",()=>console.log("Connected to the DB!"))
