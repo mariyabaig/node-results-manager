75843ab8dddcab7c0f0bbc69ff720ea085e9743a
mariyabaig <mariyabaig0106@gmail.com>
2023-06-12 11:50:11 +0530

    fixed marks adding

--- controllers/marksController.js
+++ controllers/marksController.js
@@ -5,21 +5,27 @@ const renderAddResultForm = (req, res) => {
   res.render("addresult");
 };
 
-// Controller to handle the form submission and add a new result
 const addResult = async (req, res) => {
-  try {
-    const { rollNumber, dob, name, totalScore } = req.body;
-
-    // Create a new marks entry
-    const marks = new Marks({ rollNumber, dob, name, totalScore });
-    await marks.save();
-
-    res.redirect("/teacher/dashboard"); // Redirect to the teacher's dashboard
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
-  }
-};
+    try {
+      const { rollNumber, dob, name, totalScore } = req.body;
+  
+      // Check if a document with the same roll number already exists
+      const existingMarks = await Marks.findOne({ rollNumber });
+      if (existingMarks) {
+        return res.status(400).json({ message: "Roll number already exists" });
+      }
+  
+      // Create a new marks entry
+      const marks = new Marks({ rollNumber, dob, name, totalScore });
+      await marks.save();
+  
+      res.redirect("/teacher/dashboard"); // Redirect to the teacher's dashboard
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server error" });
+    }
+  };
+  
 
 module.exports = {
   renderAddResultForm,
