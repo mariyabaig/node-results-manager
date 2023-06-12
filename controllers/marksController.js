6204f6c7a3ad8d6247fe29b2b442ca7e89aab6bd
mariyabaig <mariyabaig0106@gmail.com>
2023-06-12 19:35:59 +0530

    CRUD added

--- controllers/marksController.js
+++ controllers/marksController.js
@@ -5,47 +5,18 @@ const renderAddResultForm = (req, res) => {
   res.render("addresult");
 };
 
-// const addResult = async (req, res) => {
-//     try {
-//       const { rollNumber, dob, name, totalScore } = req.body;
-  
-//       // Check if a document with the same roll number already exists
-//       const existingMarks = await Marks.findOne({ rollNumber });
-//       if (existingMarks) {
-//         return res.status(400).json({ message: "Roll number already exists" });
-//       }
-  
-//       // Create a new marks entry
-//       const marks = new Marks({ rollNumber, dob, name, totalScore });
-//       await marks.save();
-  
-//       res.redirect("/teacher/dashboard"); // Redirect to the teacher's dashboard
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ message: "Server error" });
-//     }
-//   };
-  
 const addResult = async (req, res) => {
   try {
     const { rollNumber, dob, name, totalScore } = req.body;
 
-    // Find the student by roll number
-    const student = await Student.findOne({ roll: rollNumber });
-
-    if (!student) {
-      return res.status(400).json({ message: "Student not found" });
+    // Check if a document with the same roll number already exists
+    const existingMarks = await Marks.findOne({ rollNumber });
+    if (existingMarks) {
+      return res.status(400).json({ message: "Roll number already exists" });
     }
 
-    // Create a new marks entry and associate it with the student
-    const marks = new Marks({
-      rollNumber,
-      dob,
-      name,
-      totalScore,
-      student: student._id, // Assign the student's ObjectId to the marks entry
-    });
-
+    // Create a new marks entry
+    const marks = new Marks({ rollNumber, dob, name, totalScore });
     await marks.save();
 
     res.redirect("/teacher/dashboard"); // Redirect to the teacher's dashboard
@@ -55,8 +26,66 @@ const addResult = async (req, res) => {
   }
 };
 
+const viewResult = async (req, res) => {
+  try {
+    const rollNumber = req.params.rollNumber;
+
+    // Find the marks entry by roll number
+    const marks = await Marks.findOne({ rollNumber });
+    if (!marks) {
+      return res.status(404).json({ message: "Marks not found" });
+    }
+
+    res.json(marks); // Return the marks entry as JSON
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const editResult = async (req, res) => {
+  try {
+    const rollNumber = req.params.rollNumber;
+    const updates = req.body;
+
+    // Find the marks entry by roll number and update it
+    const marks = await Marks.findOneAndUpdate({ rollNumber }, updates, {
+      new: true,
+    });
+
+    if (!marks) {
+      return res.status(404).json({ message: "Marks not found" });
+    }
+
+    res.json(marks); // Return the updated marks entry as JSON
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const deleteResult = async (req, res) => {
+  try {
+    const rollNumber = req.params.rollNumber;
+
+    // Find the marks entry by roll number and delete it
+    const marks = await Marks.findOneAndDelete({ rollNumber });
+
+    if (!marks) {
+      return res.status(404).json({ message: "Marks not found" });
+    }
+
+    res.json({ message: "Marks deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 module.exports = {
   renderAddResultForm,
   addResult,
+  viewResult,
+  editResult,
+  deleteResult,
 };
