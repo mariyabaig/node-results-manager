56ae8ca9eefe60ef32c8722cc7dd15b9f2630ec4
mariyabaig <mariyabaig0106@gmail.com>
2023-06-13 13:30:05 +0530

    action routes fixed

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -4,8 +4,10 @@ const marksController = require("../controllers/marksController");
 
 // GET route to render the add result form
 router.get("/addresult", marksController.renderAddResultForm);
+router.post("/addresult", marksController.renderAddResultForm);
 
 // POST route to handle the form submission and add a new result
+router.get("/addresult", marksController.addResult);
 router.post("/addresult", marksController.addResult);
 
 module.exports = router;
