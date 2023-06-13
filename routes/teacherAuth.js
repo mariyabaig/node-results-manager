7d9bd3fcdbcf664283a733da12e7ca2098e23b67
mariyabaig <mariyabaig0106@gmail.com>
2023-06-13 13:30:32 +0530

    teacher authentication routes corrected

--- routes/teacherAuth.js
+++ routes/teacherAuth.js
@@ -2,27 +2,36 @@ const express = require("express");
 const router = express.Router();
 const teacherAuth = require("../controllers/teacherAuth");
 
-// Registration route
-router.get("/register", (req, res) => {
-  res.render("teacher/register"); // Assuming you have a "register.ejs" file in your views folder
-});
-
+router.get("/register", teacherAuth.registerController);
 router.post("/register", teacherAuth.registerController);
 
-// Login route
-router.get("/button",(req,res)=>{
-    res.render("teacher/button")
-})
-router.get("/login", (req, res) => {
-  res.render("teacher/login"); // Assuming you have a "login.ejs" file in your views folder
-});
+// <<<<<<< HEAD
+// // Login route
+// router.get("/button",(req,res)=>{
+//     res.render("teacher/button")
+// })
+// router.get("/login", (req, res) => {
+//   res.render("teacher/login"); // Assuming you have a "login.ejs" file in your views folder
+// });
 
-router.get("/dashboard", (req, res) => {
-  res.render("teacher/dashboard"); // Assuming you have a "login.ejs" file in your views folder
-});
+// router.get("/dashboard", (req, res) => {
+//   res.render("teacher/dashboard"); // Assuming you have a "login.ejs" file in your views folder
+// });
 
+// router.post("/login", teacherAuth.loginController);
+// router.post("/dashboard", teacherAuth.dashboardController);
+// =======
+router.get("/login", teacherAuth.loginController);
 router.post("/login", teacherAuth.loginController);
+
+router.get("/dashboard", teacherAuth.dashboardController);
 router.post("/dashboard", teacherAuth.dashboardController);
 
-module.exports = router;
+router.get("/portal");
+router.post("/portal");
 
+// // router.get("/addmarks", teacherAuth.addMarksController);
+// // router.post("/addmarks", teacherAuth.addMarksController);
+// >>>>>>> 41a6c35 (teacher authentication routes corrected)
+
+module.exports = router;
