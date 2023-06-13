5c4c9d4533571ec3116433b5a6b54bd5b282669b
mariyabaig <mariyabaig0106@gmail.com>
2023-06-13 20:43:18 +0530

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./views/**/*.ejs,ejs,html,css,js,jsx'], // Update this array with the paths to your EJS template files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
