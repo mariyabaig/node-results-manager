211bc9363f7cb5dbd490dc2665916cacb911c3a8
mariyabaig <mariyabaig0106@gmail.com>
2023-06-13 15:27:10 +0530

    endpoint removed for forms

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const marksController = require("../controllers/marksController");
 
 // GET route to render the add result form
-router.get("/addresult", marksController.renderAddResultForm);
-router.post("/addresult", marksController.renderAddResultForm);
+// router.get("/addresult", marksController.renderAddResultForm);
+// router.post("/addresult", marksController.renderAddResultForm);
 
 // POST route to handle the form submission and add a new result
 router.get("/addresult", marksController.addResult);
