1208379a078b601150eb4b96a094fa890f143e08
mariyabaig <mariyabaig0106@gmail.com>
2023-06-16 01:30:42 +0530

    teacher auth updated

--- controllers/teacherAuth.js
+++ controllers/teacherAuth.js
@@ -22,63 +22,139 @@ const registerController = async (req, res) => {
 
 const loginController = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    console.log("Email:", req.body.email);
+    console.log("Password:", req.body.password);
 
-    // Check if the email exists
-    const teacher = await Teacher.findOne({ email });
+    const teacher = await Teacher.findOne({ email: req.body.email });
+    console.log(teacher);
     if (!teacher) {
       return res.render("teacher/login", { error: "Invalid email or password" });
     }
 
-    // Compare the entered password with the stored hashed password
-    const isPasswordValid = await bcrypt.compare(password, teacher.password);
+    const isPasswordValid = await bcrypt.compare(req.body.password, teacher.password);
+    console.log(isPasswordValid)
     if (!isPasswordValid) {
       return res.render("teacher/login", { error: "Invalid email or password" });
     }
-
-    // Set up session or authentication token for the logged-in teacher
-
-    // Redirect to the teacher's dashboard upon successful login
-    res.render("teacher/dashboard", { teacher });
-  } catch (error) {
-    res.render("teacher/login", { error: "Server error" });
-  }
-};
-
-// Dashboard controller
-const dashboardController = async (req, res) => {
-  try {
-    // Assuming you have a session-based authentication system in place,
-    // you can access the authenticated teacher's information from the session
-    const { email } = req.session.teacher;
-
-    // Find the teacher by email in the database
-    const teacher = await Teacher.findOne({ email });
-
-    // You can pass the teacher's information to the dashboard view
-    res.render("teacher/dashboard", { teacher });
+    console.log("Password is valid!");
+    res.redirect("/teacher/dashboard");
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server error" });
+    res.render("teacher/login", { error: "An error occurred during login" });
   }
 };
 
-// Add marks controller
-// const addMarksController = async (req, res) => {
+// const loginController = async (req, res) => {
 //   try {
-//     const { name, dob, totalScore, rollNumber } = req.body;
-//     const marksData = new Marks({ name, dob, totalScore, rollNumber });
-//     await marksData.save();
-//     res.status(200).json({ message: "Marks added successfully" });
+//     //const { email, password } = req.body;
+//     console.log("Email:", req.body.email);
+//     console.log("Password:", req.body.password);
+
+//     // Check if the email exists
+//     const teacher = await Teacher.findOne({ email: req.body.email });
+//     console.log(teacher); // Log the teacher object
+//     if (!teacher) {
+//       return res.render("teacher/login", { error: "Invalid email or password" });
+//     }
+
+//     // Compare the entered password with the stored hashed password
+//     const isPasswordValid = await bcrypt.compare(req.body.password, req.body.teacher.password);
+//     if (!isPasswordValid) {
+//       return res.render("teacher/login", { error: "Invalid email or password" });
+//     }
+
+//     // Set up session or authentication token for the logged-in teacher
+//     req.session.teacher = teacher;
+//     // Redirect to the teacher's dashboard upon successful login
+//     res.redirect("/teacher/dashboard");
+//   } catch (error) {
+//     console.error(error);
+//     res.render("teacher/login", { error: "An error occurred during login" });
+//   }
+// };
+
+
+// const loginController = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+//     console.log("Email:", email);
+//     console.log("Password:", password);
+
+//     // Check if the email exists
+//     const teacher = await Teacher.findOne({ email });
+//     if (!teacher) {
+//       return res.render("teacher/login", { error: "Invalid email or password" });
+//     }
+
+//     // Compare the entered password with the stored hashed password
+//     const isPasswordValid = await bcrypt.compare(password, teacher.password);
+//     if (!isPasswordValid) {
+//       return res.render("teacher/login", { error: "Invalid email or password" });
+//     }
+
+//     // Set up session or authentication token for the logged-in teacher
+
+//     // Redirect to the teacher's dashboard upon successful login
+//     res.redirect("/teacher/dashboard", { teacher });
+//   } catch (error) {
+//     console.error(error);
+//     res.render("teacher/login", { error: "An error occurred during login" });
+//   }
+// };
+
+
+
+// const loginController = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     // Check if the email exists
+//     const teacher = await Teacher.findOne({ email });
+//     if (!teacher) {
+//       return res.render("teacher/login", { error: "Invalid email or password" });
+//     }
+
+//     // Compare the entered password with the stored hashed password
+//     const isPasswordValid = await bcrypt.compare(password, teacher.password);
+//     if (!isPasswordValid) {
+//       return res.render("teacher/login", { error: "Invalid email or password" });
+//     }
+
+//     res.redirect("/teacher/dashboard", { teacher });
+//   } catch (error) {
+//     res.render("teacher/login", { error: "Server error" });
+//   }
+// };
+
+// Dashboard controller
+// const dashboardController = async (req, res) => {
+//   try {
+   
+//     const { email } = req.session.teacher;
+//     const teacher = await Teacher.findOne({ email });
+//     res.render("teacher/dashboard", { teacher });
 //   } catch (error) {
 //     console.error(error);
 //     res.status(500).json({ message: "Server error" });
 //   }
 // };
 
+// const dashboardController = async (req, res) => {
+//   try {
+//     const { email } = req.session.teacher || {};
+//     const teacher = await Teacher.findOne({ email: req.body.email });
+//     res.render("teacher/dashboard", { teacher });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// };
+
+
+
 module.exports = {
   registerController,
   loginController,
-  dashboardController, 
+  // dashboardController, 
   // addMarksController
 };
