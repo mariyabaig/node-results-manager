b6c822a2968dc90e225cbcdf64d739571cf1d3d9
mariyabaig <mariyabaig0106@gmail.com>
2023-06-16 21:55:19 +0530

    CRUD routes added

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,7 +1,36 @@
+// const express = require("express");
+// const router = express.Router();
+// const teacherAuth = require("../controllers/teacherAuth");
+// const bodyParser = require("body-parser");
+
+// // Parse request bodies as JSON
+// router.use(bodyParser.json());
+// router.use(bodyParser.urlencoded({ extended: true }));
+
+// router.get("/register", teacherAuth.registerController);
+// router.post("/register", teacherAuth.registerController);
+
+// router.get("/login", teacherAuth.loginController);
+// router.post("/login", teacherAuth.loginController);
+
+// // router.get("/dashboard", (req, res) => {
+// //   res.render("teacher/dashboard");
+// // });
+
+// router.get("/portal", (req, res) => {
+//   res.render("teacher/portal");
+// });
+// router.post("/portal", (req, res) => {
+//   // Handle the POST request for "/portal" here
+// });
+
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const teacherAuth = require("../controllers/teacherAuth");
 const bodyParser = require("body-parser");
+const marksController = require("../controllers/marksController");
 
 // Parse request bodies as JSON
 router.use(bodyParser.json());
@@ -13,9 +42,6 @@ router.post("/register", teacherAuth.registerController);
 router.get("/login", teacherAuth.loginController);
 router.post("/login", teacherAuth.loginController);
 
-router.get("/dashboard");
-router.post("/dashboard");
-
 router.get("/portal", (req, res) => {
   res.render("teacher/portal");
 });
@@ -23,5 +49,25 @@ router.post("/portal", (req, res) => {
   // Handle the POST request for "/portal" here
 });
 
+// Route to render the add result form
+router.get("/addresult", teacherAuth.addResult);
+
+// Route to handle adding a result
+router.post("/addresult", teacherAuth.addResult);
+
+// Route to view a specific result
+router.get("/result/:rollNumber", teacherAuth.viewResult);
+
+// Route to edit a specific result
+router.put("/result/:rollNumber", teacherAuth.editResult);
+
+// Route to delete a specific result
+router.delete("/result/:rollNumber", teacherAuth.deleteResult);
+
+// Route to render the teacher's dashboard
+router.get("/dashboard", teacherAuth.renderDashboard);
+
+// Route to view all the results
+router.get("/results", teacherAuth.viewAllResults);
 
 module.exports = router;
