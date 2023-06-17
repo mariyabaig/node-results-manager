8578cfb13bc41aeeb0d1f650fc222c654da653af
mariyabaig <mariyabaig0106@gmail.com>
2023-06-17 20:16:37 +0530

    body parser added

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const studentController = require("../controllers/studentController");
+const bodyParser = require("body-parser");
 
+
+// Parse request bodies as JSON
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 // Route for student login
 router.get("/login", (req, res) => {
   res.render("student/login");
