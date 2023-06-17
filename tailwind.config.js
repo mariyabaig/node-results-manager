427cdcdaaff8bac66833ab9056acab0ebe9c3d4e
mariyabaig <mariyabaig0106@gmail.com>
2023-06-17 23:29:57 +0530

    added styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/*.{html,js,css} ",
-    "./views/*.ejs",
+    "./views/*.ejs",'./views/**/*.ejs',
   
   ], // Update this array with the paths to your EJS template files
   theme: {
